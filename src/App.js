0841b8a2d902d1d78531a5a843aa9f58b0ffb998
dickie10 <thapasuyash@gmail.com>
2021-07-05 23:10:23 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react' 
+import CitySelector from './CitySelector';
+import './App.css'; 
+import {Container} from 'react-bootstrap'; 
+import UseFetch from './UseFetch'; 
+import {API_Key, Base} from './keys'; 
+import WeatherList from './WeatherList'; 
+const App = () => { 
+  const {data,error, isLoading, setUrl} = UseFetch(); 
+  const getcontent = () =>{ 
+    if (error) return <h2>Error when fetching: {error}</h2> 
+    if(!data && isLoading) return <h2>LOADING..</h2> 
+    if(!data) return null; 
+    return <WeatherList weathers={data.list} />
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App"> 
+       <CitySelector onSearch={(city) => setUrl(`${Base}/data/2.5/forecast?q=${city}&cnt=5&appid=${API_Key}&units=metric`)}/>  
+          {getcontent()}
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/keys.js
@@ -0,0 +1,4 @@
+module.exports = { 
+    API_Key : "9b7f552245c27b4239131b02133b0eb3", 
+    Base :'https://api.openweathermap.org/',
+}; 
